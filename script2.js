1db1c3d4100a139c3d6c5b24f84b7217cddd7782
Edward J <edjeong99@gmail.com>
2023-08-26 18:33:18 -0500

    bb

--- script2.js
+++ script2.js
@@ -3,6 +3,7 @@ console.log("start");
 let budget = 500;
 let spent = 400;
 let goalamount = 100;
+let expense = 0;
 
 const balance = document.getElementById("balance");
 const goal = document.getElementById("goal");
@@ -47,12 +48,12 @@ function addTransaction(e) {
 function updateValues() {
   // Loop through the transactions array and create a new array with only amounts
   const amounts = transactions.map((transaction) => transaction.amount);
-  let expense = expense + amount.value;
-  let balance = goalamount - expense;
+  expense = expense + amount.value;
+  let balanceAmount = goalamount - expense;
 
   const total = amounts.reduce((acc, val) => (acc += val), 0).toFixed(2);
 
-  balance.innerHTML = `$${total}`;
+  balance.innerHTML = `$${balanceAmount}`;
   goal.innerHTML = `$${goalamount}`;
   money_minus.innerHTML = `$${expense}`;
 }
